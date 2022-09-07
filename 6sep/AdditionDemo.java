062fbcd3681c3fa2eeeaaeb9cb950034c269bb5f
Shuruti Singhal <110531820+Shuruti1234@users.noreply.github.com>
2022-09-07 14:42:08 +0530

    Add files via upload

---
+++ 6sep/AdditionDemo.java
@@ -0,0 +1,32 @@
+/*
+Program: Add 2 numbers
+@author: Shuruti Singhal
+@Date: 6 sep 2022
+*/
+
+//decalraing a class
+class AdditionDemo
+{
+	//calling main
+	
+  public static void main(String[] args)
+  {
+	  /* int a,b;
+	  a=89;
+	  b=56;
+	  System.out.println(a+b); */
+	  int num1= Integer.parseInt(args[0]);
+	  int num2= Integer.parseInt(args[1]);
+	 
+	  //System.out.println(num1+num2);
+	  
+	 System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
+	 
+	 
+	 
+  }
+ // end of main
+
+}
+// end of class AdditionDemo
+
