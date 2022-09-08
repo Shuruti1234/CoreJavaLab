7b13ea1eccc77cc3ec6a94b70e8193a92bd14c33
Shuruti Singhal <110531820+Shuruti1234@users.noreply.github.com>
2022-09-08 14:16:22 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,34 @@
+/*
+Program: UndercoreLiteral
+@author: Shuruti Singhal
+@Date: 6 sep 2022
+*/
+
+//decalraing a class
+
+
+ class A
+{
+  
+	
+} 
+ 
+class LiteralDemo
+{
+	//calling main
+	
+	
+  public static final void main(String[] args)
+  {
+	  int number=90;
+	  System.out.println("Number is :"+number);
+	 
+  }
+  
+
+ 
+ // end of main
+
+}
+// end of class LiteralDemo
+
---
+++ VersionDemo.java
@@ -0,0 +1,54 @@
+/*
+Program: Current version of java
+@author: Shuruti Singhal
+@Date: 6 sep 2022
+*/
+
+//declaring a class
+
+class VersionDemo
+{
+	//calling main
+	
+	
+  public static final void main(String[] args)
+  {
+	  float divisor=8;
+	  float dividend= 67;
+	  float q= dividend/divisor;
+	  int r =(int)(dividend%divisor);
+	  
+	  boolean status= 10>9;
+	  
+	  int num1,num2;
+	  
+	   num1=num2=3;
+	   
+	   //int num1=num2=3;
+	   
+	   int num1,num2=3;
+	   int num1;
+	   int num2=3;
+	   //////////jghjgjhgjy
+	   
+	  
+	  int a =3;//assigning the value
+	   boolean f= (a==3);// comparing whether a is 3 ->true/false
+	  
+	  
+	  System.out.println("The status is :"+status);
+	  
+	  System.out.println("The quotient is :"+q);
+	   System.out.println("The remainder is :"+r);
+	  
+	  System.out.println("Java version is :"+ System.getProperty("java.version"));
+	 
+  }
+  
+
+ 
+ // end of main
+
+}
+// end of class VersionDemo
+
