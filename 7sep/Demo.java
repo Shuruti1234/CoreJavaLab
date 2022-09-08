61c3bdf0ef17bb6186414f71a9ed9a2dd2297ade
Shuruti Singhal <110531820+Shuruti1234@users.noreply.github.com>
2022-09-08 14:17:23 +0530

    Add files via upload

---
+++ 7sep/Demo.java
@@ -0,0 +1,24 @@
+
+class Demo
+{
+
+   public static void main(String args[])
+   {
+      short b1=7;
+	  short b2=9;
+	  short sum;
+	  sum= (short)(b1+b2);
+	  
+	  System.out.println("The sum is :"+ sum);
+	  
+	  double f1=7.98;
+	  double f2= 8.33;
+	  float f3= (float)(f1+f2);
+	  
+	  System.out.println("The sum is :"+ f3);
+   
+   
+   }
+
+
+}
